06e292cdca483cdde9bac55a6dd89d03ce53b374
PatrykRoskosz <roskoszpatryk55@gmail.com>
2023-07-11 16:52:49 +0200

    add plugins

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,51 @@
-import ReactDOM from "react-dom";
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect, useRef } from "react";
+import { createRoot } from "react-dom/client";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
-	return <h1>Hi there</h1>;
+	const ref = useRef<any>();
+	const [input, setInput] = useState("");
+	const [code, setCode] = useState("");
+
+	const startService = async () => {
+		ref.current = await esbuild.startService({
+			worker: true,
+			wasmURL: "/esbuild.wasm",
+		});
+	};
+
+	useEffect(() => {
+		startService();
+	}, []);
+
+	const onClick = async () => {
+		if (!ref.current) {
+			return;
+		}
+		const result = await ref.current.build({
+			entryPoints: ["index.js"],
+			bundle: true,
+			write: false,
+			plugins: [unpkgPathPlugin()],
+		});
+		console.log(result);
+		setCode(result.code);
+	};
+
+	return (
+		<div>
+			<textarea
+				value={input}
+				onChange={e => setInput(e.target.value)}></textarea>
+			<div>
+				<button onClick={onClick}>Submit</button>
+			</div>
+			<pre>{code}</pre>
+		</div>
+	);
 };
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
-)
+const rootElement = document.querySelector("#root") as HTMLElement;
+const root = createRoot(rootElement);
+root.render(<App />);
