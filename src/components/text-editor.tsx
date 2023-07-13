ffd256f7b2a2a52a7a592eef9c289c0ba9e6ec4e
PatrykRoskosz <roskoszpatryk55@gmail.com>
2023-07-13 22:36:28 +0200

    add styles to text editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,10 +1,46 @@
-import { useState } from "react"
-import MDEditor from "@uiw/react-md-editor"
+import { useState, useEffect, useRef } from "react";
+import MDEditor from "@uiw/react-md-editor";
+
+import "./text-editor.css";
 
 const TextEditor: React.FC = () => {
-const [value, setValue] = useState('')
-    return <div>
-        <MDEditor.Markdown source={'# Header'}/>
-    </div>
-}
-export default TextEditor
+	const [editing, setEditing] = useState(false);
+	const [value, setValue] = useState("# Header");
+	const ref = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		const listener = (event: MouseEvent) => {
+			if (
+				ref.current &&
+				event.target &&
+				ref.current.contains(event.target as Node)
+			) {
+				return;
+			}
+
+			setEditing(false);
+		};
+		document.addEventListener("click", listener, { capture: true });
+
+		return () => {
+			document.removeEventListener("click", listener, { capture: true });
+		};
+	}, []);
+
+	if (editing) {
+		return (
+			<div ref={ref} className='text-editor'>
+				<MDEditor value={value} onChange={v => setValue(v || "")} />
+			</div>
+		);
+	}
+
+	return (
+		<div onClick={() => setEditing(true)} className='text-editor card'>
+			<div className="card-content">
+				<MDEditor.Markdown source={value} />
+			</div>
+		</div>
+	);
+};
+export default TextEditor;
