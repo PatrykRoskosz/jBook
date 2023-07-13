c9c325892563c666068a7da91313e5c94e551119
PatrykRoskosz <roskoszpatryk55@gmail.com>
2023-07-13 11:57:52 +0200

    add text format functionality

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -6,7 +6,7 @@ const fileCache = localForage.createInstance({
 	name: "fileCache",
 });
 
-export const fetchPlugin = (inputCode: string) => {
+export const fetchPlugin = (inputCode: string | undefined) => {
 	return {
 		name: "fetch-plugin",
 		setup(build: esbuild.PluginBuild) {
