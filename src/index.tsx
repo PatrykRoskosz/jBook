a9bd9daeb6541dbed4f5850e27b7f782cb2a5c1a
PatrykRoskosz <roskoszpatryk55@gmail.com>
2023-07-19 12:42:08 +0200

    add progresbar and fix background

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import ReactDOM from "react-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./state";
 import CellList from "./components/cell-list";
 
+const rootElement = document.querySelector("#root") as HTMLElement;
+const root = createRoot(rootElement);
 const App = () => {
 	return (
 		<Provider store={store}>
@@ -15,4 +17,4 @@ const App = () => {
 	);
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+root.render(<App />);
